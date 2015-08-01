d8e39268e42b400db5c26c6d64b8dca641c702d2
Joci5525 <olah.joci.3123@gmail.com>
2015-08-01 18:58:28 +0200

    Update FirstPlugin.php
    Trying something else

--- src/joci5525/firstplugin/FirstPlugin.php
+++ src/joci5525/firstplugin/FirstPlugin.php
@@ -5,5 +5,23 @@ namespace joci5525\firstplugin;
 use pocketmine\plugin\PluginBase;
 
 class FirstPlugin extends PluginBase{
+    public function onEnable()
+     $this->getLogger()->info("FirstPlugin has been  enabled");
+   } 
     
 }
+
+public function onDisable(){
+    $this->getLogger()->info("FirstPlugin has been  disabled");
+  }
+}
+public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+		switch($cmd->getName()){
+			case "Tutorial":
+        			if($sender->hasPermission("tutorial.command.howto") or $sender->hasPermission("tut.command") or $sender->hasPermission("skywars")) { 
+					$sender->sendMessage("----FirstPlugin----");
+					$sender->sendMessage("/tut = Joci5525 ");
+					return true;
+        			}else{
+        				$sender->sendMessage("You haven't the permission to run this command");
+        			}
